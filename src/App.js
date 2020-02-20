bea189510b6b7dbb43a1fa1de724326bac7bccbb
Zachary Hislop <hislopzach@gmail.com>
2020-02-20 00:36:26 -0800

    working basic camviewer based on url

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Player, ControlBar } from "video-react";
+import HLSSource from "./HLSSource";
+import "video-react/dist/video-react.css";
+import "./App.css";
 
 function App() {
+  const url = "https://cams.cdn-surfline.com/cdn-int/au-kirra/playlist.m3u8";
+  const getUrl = () => {
+    let search = window.location.search;
+    let params = new URLSearchParams(search);
+    return params.get("streamUrl");
+  };
+
+  useEffect(() => {
+    setVideoUrl(getUrl());
+  }, []);
+  const [videoUrl, setVideoUrl] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Player>
+        <HLSSource isVideoChild src={videoUrl} />
+        <ControlBar></ControlBar>
+      </Player>
     </div>
   );
 }
